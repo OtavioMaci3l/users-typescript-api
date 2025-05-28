00af9185d850ccf7b9af767e5c9e7de0de9ac5de
Otávio Augusto Maciel Alves <otaviomaci3lalves@gmail.com>
2025-05-28 20:36:28 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepositories implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "string",
-        lastName: "string",
-        email: "string",
-        password: "string",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("/users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
