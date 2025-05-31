8eef5d478fbc795870adeed5c9fa5ab5f0cb41b3
Otávio Augusto Maciel Alves <otaviomaci3lalves@gmail.com>
2025-05-31 18:54:10 -0300

    feat: add HttpStatusCode enum

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -1,13 +1,22 @@
-import { HttpResponse } from "./protocols";
+import { HttpResponse, HttpStatusCode } from "./protocols";
 
-export const ok = <T>(body: T): HttpResponse<T> => ({ statusCode: 200, body: body });
+export const ok = <T>(body: T): HttpResponse<T> => ({
+  statusCode: HttpStatusCode.OK,
+  body: body,
+});
 
-export const created = <T>(body: T): HttpResponse<T> => ({ statusCode: 201, body: body });
+export const created = <T>(body: T): HttpResponse<T> => ({
+  statusCode: HttpStatusCode.CREATED,
+  body: body,
+});
 
 export const badRequest = (message: string): HttpResponse<string> => {
-  return { statusCode: 400, body: message };
+  return { statusCode: HttpStatusCode.BAD_REQUEST, body: message };
 };
 
 export const serverError = (): HttpResponse<string> => {
-    return { statusCode: 500, body: "Something went wrong" }
-}
+  return {
+    statusCode: HttpStatusCode.SERVER_ERROR,
+    body: "Something went wrong",
+  };
+};
